bb41395cc36eab6a4acb2189fabaf37ff36b7f96
Serhii Okuniev <sergiosis@ukr.net>
2022-11-09 00:56:34 +0200

    make some fix and install styled-components

---
+++ src/components/Friend/Friends.jsx
@@ -0,0 +1,40 @@
+const Friends = () => {
+    
+    const arr = [{ title: 'ЛКП "ЛЕВ"', time: '8-19', adress: 'Grigorenka Street, 25', email: 'barbos@gmail.com', phone: '0664880480' }, { title: 'Барбос', time: '8-20', adress: 'Grigorenka Street, 3', email: 'barbos@gmail.com', phone: '4880480' }, { title: 'ЛКП "ЛЕВ"', time: '', adress: '', email: 'barbos@gmail.com', phone: '0664880480' }];
+
+    return (
+        <>
+            <h2>Our friends</h2>
+            {arr.map(({title, time, adress, email, phone}) => (
+                    <div>
+                        <h3>{title}</h3>
+                        <ul>
+                            <li>Time:
+                                {
+                                    time ? (<span>{time}</span>) : (<span>----------</span>)
+                                }
+                            </li>
+                            <li>Adress:
+                                {
+                                    adress ? (<span>{adress}</span>) : (<span>----------</span>)
+                                }
+                            </li>
+                            <li>Email:
+                                {
+                                    email ? (<span>{email}</span>) : (<span>----------</span>)
+                                }
+                            </li>
+                            <li>Phone:
+                                {
+                                    phone ? (<span>{phone}</span>) : (<span>----------</span>)
+                                }
+                            </li>
+                        </ul>
+                    </div>
+                ))
+            }
+        </>
+    )
+}
+
+export default Friends;
---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,13 @@
+import Friends from "components/Friend/Friends"
+
+const MainPage = () => {
+
+    return (
+        <>
+            <Friends/>
+        </>
+
+    )
+}
+
+export default MainPage
