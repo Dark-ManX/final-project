6c028ba50462b73d4084eddeb4a4492d87da20fb
Aleksandr Kravchuk <alexandrokrk@gmail.com>
2022-11-09 15:47:20 +0200

    1

---
+++ src/routes/routes.js
@@ -0,0 +1,55 @@
+export const ROUTES = {
+  AUTH: {
+    // POST
+    create: '/register',
+    // PATCH
+    register: '/register/:id',
+    // POST
+    login: '/login',
+    // GET
+    logout: '/logout',
+    // GET
+    current: '/current',
+    // PATCH
+    updateInfoUser: '/user',
+  },
+  FRIENDS: {
+    // GET
+    getAllFriends: '/friends',
+    //   GET
+    getFriendById: '/friends/:id',
+  },
+  NEWS: {
+    //   GET
+    getAllNews: '/news',
+    //   GET
+    getNewsById: '/news/:id',
+    // GET
+    getSearchNews: '/news/search/:value',
+  },
+  USER: {
+    //   GET
+    getUserInfo: '/user',
+    // PATCH
+    updateInfoUser: '/user',
+    //   GET
+    getUserPets: '/pets',
+    // POST
+    createUserPet: '/pets',
+    // PATCH
+    addUserPet: '/pets/:id',
+    // DELETE
+    deleteUserPet: '/pets/:id',
+  },
+  NOTICES: {
+    //   GET
+    //   value one of (sell,for-free,lost-found)
+    getNoticesByCategory: '/notices/:value',
+    //   GET
+    getNoticeById: '/notices/:id',
+    //   GET
+    getNoticesLostFound: '/notices/:lost-found',
+    //   GET
+    getNoticeById: '/notices/one/:id',
+  },
+};
