190bd5fe7b12a62420ab5a2f634727fbb53fa55f
Serhii Okuniev <sergiosis@ukr.net>
2022-11-11 15:37:25 +0200

    resolve

--- src/pages/OurFriendsPage/OurFriendsPage.jsx
+++ src/pages/OurFriendsPage/OurFriendsPage.jsx
@@ -2,7 +2,8 @@ import shortid from 'shortid';
 import { Container, FriendsThumb, FirstThumb, Title } from "./OurFriendsPage.styled";
 
 
-const OurFriendsPage = () => {
+const OurFriendsPage = () => { 
+    
     
     const arr = [{ title: 'ЛКП "ЛЕВ"', time: '8-19', adress: 'Grigorenka Street, 25', email: 'barbos@gmail.com', phone: '0664880480' }, { title: 'Барбос', time: '8-20', adress: 'Grigorenka Street, 3', email: 'barbos@gmail.com', phone: '4880480' }, { title: 'ЛКП "ЛЕВ"', time: '', adress: '', email: 'barbos@gmail.com', phone: '0664880480' }];
 
