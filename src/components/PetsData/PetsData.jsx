276487e9c3cede4ba95e4c05ec32eede964643b2
OksanaOstafiichuk <Ostafiichukk@gmail.com>
2022-11-12 15:39:33 +0200

    fix token

--- src/components/PetsData/PetsData.jsx
+++ src/components/PetsData/PetsData.jsx
@@ -3,11 +3,9 @@ import { useState, useEffect } from 'react';
 
 import { PetsList } from 'components/PetsList/PetsList';
 import { ROUTES } from '../../routes/routes';
-const AUTH_TOKEN =
-  'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNjdiNWYwMTU1Y2U0MjJkZTI4NGYyNyIsImlhdCI6MTY2ODI2Mjg2NywiZXhwIjoxNjY4Mjk4ODY3fQ.9o04ogrS8yf81kU3p4DJOBL1zvXdhE-3skeHwELZFUY';
 
 axios.defaults.baseURL = ROUTES.BASE_URL;
-axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+axios.defaults.headers.common['Authorization'] = 'AUTH_TOKEN';
 
 const getPets = () => {
   const response = axios.get(`${ROUTES.USER.getUserPets}`);
--- src/components/UserData/UserData.jsx
+++ src/components/UserData/UserData.jsx
@@ -4,11 +4,9 @@ import { useState, useEffect } from 'react';
 import { UserDataItem } from 'components/UserDataItem/UserDataItem';
 
 import { ROUTES } from '../../routes/routes';
-const AUTH_TOKEN =
-  'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNjdiNWYwMTU1Y2U0MjJkZTI4NGYyNyIsImlhdCI6MTY2ODI2Mjg2NywiZXhwIjoxNjY4Mjk4ODY3fQ.9o04ogrS8yf81kU3p4DJOBL1zvXdhE-3skeHwELZFUY';
 
 axios.defaults.baseURL = ROUTES.BASE_URL;
-axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+axios.defaults.headers.common['Authorization'] = 'AUTH_TOKEN';
 
 const getUser = () => {
   const response = axios.get(`${ROUTES.USER.getUserInfo}`);
