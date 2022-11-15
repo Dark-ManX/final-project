0a9948aa0862215a252ac9cd4edd628f3c19813c
Dark-ManX <93794893+Dark-ManX@users.noreply.github.com>
2022-11-15 18:22:34 +0200

    Update RegisterPage.jsx

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,8 +1,6 @@
 import { useCreateUserMutation } from 'redux/auth/authOperations';
 import { useNavigate } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
-// import { Link } from 'react-router-dom';
-// import axios from 'axios';
 import RegistrationDetails from 'components/RegistrationDetails';
 
 const Registration = () => {
@@ -10,42 +8,20 @@ const Registration = () => {
   const [password, setPassword] = useState('');
   const [userId, setUserId] = useState('');
 
-  // const [username, setUsername] = useState('');
-  // const [city, setCity] = useState('');
-  // const [phone, setPhone] = useState('');
-
-  // const [searchParams, setSearchParams] = useSearchParams();
   const navigate = useNavigate();
 
-  // const [registerNewUser] = useRegisterUserMutation();
   const [createNewUser] = useCreateUserMutation();
-
-  // const registrationDetails = { city, name, phone };
 
   const handleChange = event => {
     const { name, value } = event.target;
     switch (name) {
       case 'email':
         setEmail(value);
-        // console.log(email);
         break;
 
       case 'password':
         setPassword(value);
-        // console.log(password);
         break;
-
-      // case 'name':
-      //   setName(value);
-      //   break;
-
-      // case 'city':
-      //   setCity(value);
-      //   break;
-
-      // case 'phone':
-      //   setPhone(value);
-      //   break;
 
       default:
         return;
@@ -75,45 +51,6 @@ const Registration = () => {
     setPassword('');
   };
 
-  // const result = async () => {
-  //   try {
-  //     const fetchUser = await fetch(
-  //       `https://team-api-blended2.herokuapp.com/register/${userId}`,
-  //       {
-  //         method: 'PATCH',
-  //         body: { name, city, phone },
-  //         headers: { 'Content-Type': 'string' },
-  //       }
-  //     );
-  //     console.log(fetchUser);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-
-  //   // try {
-  //   //   const data = await axios.patch(
-  //   //     `https://team-api-blended2.herokuapp.com/register/${userId}`,
-  //   //     registrationDetails
-  //   //   );
-  //   //   return data;
-  //   //   console.log(data);
-  //   // } catch (error) {
-  //   //   console.log(error.message);
-  //   // }
-  // };
-
-  // const handleRegisterSubmit = event => {
-  //   event.preventDefault();
-  //   result();
-  //   resetRegister();
-  // };
-
-  // const resetRegister = () => {
-  //   setName('');
-  //   setCity('');
-  //   setPhone('');
-  // };
-
   useEffect(() => {}, [userId]);
 
   return (
@@ -136,7 +73,6 @@ const Registration = () => {
           />
 
           <button type="submit">
-            {/* <navigate to={`/register/${userId}`} /> */}
             Submit
           </button>
         </form>
