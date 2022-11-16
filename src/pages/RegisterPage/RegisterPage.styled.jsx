8f17ebdf3c24b7d4e0bdb439dec65836cd9be61c
Iryna <93982819+IrinaSkrytska@users.noreply.github.com>
2022-11-16 12:10:05 +0200

    Update RegisterPage.styled.jsx

--- src/pages/RegisterPage/RegisterPage.styled.jsx
+++ src/pages/RegisterPage/RegisterPage.styled.jsx
@@ -1,1 +1,87 @@
-// import styled from "styled-components";
+import styled from 'styled-components';
+import bigWave from '../../img/bigWave.jpg';
+
+export const Img = styled.img`
+  background-image: url(${bigWave});
+  background-repeat: no-repeat;
+`;
+export const Container = styled.div`
+  margin: 0 auto;
+  margin-top: 55px;
+  width: 618px;
+  height: 541px;
+  left: 331px;
+  top: 112px;
+  background: #ffffff;
+  box-shadow: 7px 4px 14px rgba(0, 0, 0, 0.11);
+  border-radius: 40px;
+`;
+export const Title = styled.h2`
+  padding-top: 60px;
+  margin-bottom: 40px;
+  font-family: 'Manrope';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 36px;
+  line-height: 49px;
+  align-items: center;
+  text-align: center;
+  letter-spacing: 0.04em;
+`;
+export const Input = styled.input`
+  padding-left: 32px;
+  margin-bottom: 16px;
+  width: 426px;
+  height: 52px;
+  background: #fdf7f2;
+  border: 1px solid rgba(245, 146, 86, 0.5);
+  border-radius: 40px;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 25px;
+  align-items: center;
+  letter-spacing: 0.04em;
+  color: rgba(17, 17, 17, 0.6);
+`;
+
+export const Form = styled.form`
+  display: flex;
+  justify-content: center;
+  justify-content: column;
+  flex-wrap: wrap;
+`;
+
+export const Button = styled.button`
+  margin-top: 24px;
+  margin-bottom: 16px;
+  width: 458px;
+  height: 48px;
+  background: #f59256;
+  border-radius: 40px;
+  border: none;
+  font-family: 'Manrope';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 27px;
+  align-items: center;
+  letter-spacing: 0.04em;
+  color: #ffffff;
+`;
+
+export const P = styled.p`
+  font-family: 'Manrope';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 16px;
+  align-items: center;
+  text-align: center;
+  letter-spacing: 0.04em;
+  color: rgba(17, 17, 17, 0.6);
+`;
+export const Span = styled.span`
+  color: rgba(48, 145, 235, 1);
+  border-bottom: 1px solid rgba(48, 145, 235, 1);
+  border-width: thin;
+`;
