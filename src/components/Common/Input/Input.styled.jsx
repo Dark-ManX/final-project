c0e9c0504ee9052e592d9e4e28bc2b3877b3c2b2
Snisarenko21 <sepalena@gmail.com>
2022-11-16 17:34:52 +0200

    input add padding

--- src/components/Common/Input/Input.styled.jsx
+++ src/components/Common/Input/Input.styled.jsx
@@ -14,6 +14,7 @@ export const Input = styled.input`
   border: 1px solid rgba(245, 146, 86, 0.5);
   border-radius: 40px;
   margin-top: 8px;
+  padding: 0px 14px;
 
   &::placeholder {
     font-family: 'Manrope';
@@ -21,7 +22,6 @@ export const Input = styled.input`
     font-weight: 400;
     font-size: 14px;
     line-height: 19px;
-    padding-left: 14px;
     color: rgba(27, 27, 27, 0.6);
   }
 `;
