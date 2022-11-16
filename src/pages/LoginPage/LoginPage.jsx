c961257aa1a9e0d97b3abaaede2e377dac76edd1
Iryna <93982819+IrinaSkrytska@users.noreply.github.com>
2022-11-16 12:25:26 +0200

    adds styles

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,7 +1,18 @@
 import { useState } from 'react';
 import { useLoginUserMutation } from 'redux/auth/authOperations';
+import {
+  Input,
+  Title,
+  Container,
+  Form,
+  Button,
+  P,
+  Span,
+} from './LoginPage.styled';
+import { Link, useLocation } from 'react-router-dom';
 
 const LoginPage = () => {
+  const location = useLocation();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [loginUser] = useLoginUserMutation();
@@ -43,23 +54,32 @@ const LoginPage = () => {
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          placeholder="email"
-          name="email"
-          value={email}
-          onChange={handleChange}
-        />
-        <input
-          type="password"
-          value={password}
-          name="password"
-          placeholder="password"
-          onChange={handleChange}
-        />
-        <button type="submit">Submit</button>
-      </form>
+      <Container>
+        <Title>Login</Title>
+        <Form onSubmit={handleSubmit}>
+          <Input
+            type="email"
+            placeholder="Email"
+            name="email"
+            value={email}
+            onChange={handleChange}
+          />
+          <Input
+            type="password"
+            value={password}
+            name="password"
+            placeholder="Password"
+            onChange={handleChange}
+          />
+          <Button type="submit">Login</Button>{' '}
+          <P>
+            Don`t have an account?`
+            <Link to={`/register`} state={{ from: location }}>
+              <Span> Register</Span>
+            </Link>
+          </P>
+        </Form>
+      </Container>
     </>
   );
 };
