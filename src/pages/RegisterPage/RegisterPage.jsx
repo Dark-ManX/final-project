da2f0be9610d4006d0b7a8add843ebace948a2b6
Iryna <93982819+IrinaSkrytska@users.noreply.github.com>
2022-11-16 12:10:21 +0200

    fix

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,4 +1,4 @@
-import { useCreateUserMutation } from 'redux/auth/authOperations';
+import { useCreateUserMutation } from '../../redux/auth/a';
 import { useNavigate, Link, useLocation } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
 // import { Link } from 'react-router-dom';
