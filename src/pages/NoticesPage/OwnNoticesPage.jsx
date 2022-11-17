4484801f307a568a1d08942a5733d014465373aa
Serhii Okuniev <sergiosis@ukr.net>
2022-11-17 20:01:13 +0200

    try to deploy on github pages

--- src/pages/NoticesPage/OwnNoticesPage.jsx
+++ src/pages/NoticesPage/OwnNoticesPage.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react';
-import { NoticesCategoryList } from 'components/NoticesCategoryList/NoticesCategoryList';
+// import { useState } from 'react';
+// import { NoticesCategoryList } from 'components/NoticesCategoryList/NoticesCategoryList';
 
 const OwnNotices = () => {
 
-    const [page, setPage] = useState(1);
-    const [notices, setNotices] = useState([]);
-    const [error, setError] = useState('');
+    // const [page, setPage] = useState(1);
+    // const [notices, setNotices] = useState([]);
+    // const [error, setError] = useState('');
     
     return (
         <>
-            {error && <p>{error.message}</p>}
+            {/* {error && <p>{error.message}</p>} */}
 
-            <NoticesCategoryList notices={notices} />
+            {/* <NoticesCategoryList notices={notices} /> */}
         </>
     );
 };
