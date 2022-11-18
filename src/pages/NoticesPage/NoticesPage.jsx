2f4b9c3edab5582983b5ff9c35340bf6c20aeec9
Serhii Okuniev <sergiosis@ukr.net>
2022-11-18 12:41:51 +0200

    still try to deploy

--- src/pages/NoticesPage/NoticesPage.jsx
+++ src/pages/NoticesPage/NoticesPage.jsx
@@ -7,12 +7,9 @@ const NoticesPage = () => {
     const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNzM0OGUyM2RhMjk5YmRlY2I2NTFlNCIsImlhdCI6MTY2ODQ5OTcwNSwiZXhwIjoxNjY4NTM1NzA1fQ.W9gK98YZ9OzenWQpIP_e6irUwwyHiAI90L2xk4_Ebmg';
     
     return ( 
-// <<<<<<< HEAD
-//         <MainContainer>
-// =======
+
         <Container>
             <Title>Find your favorite pet</Title>
-{/* >>>>>>> 28f11a0e49f08daf093dbe3b0418d054de29436b */}
             <Nav>
                 <LinkContainer>
                     <Link to="lost-found">Lost/found</Link>
