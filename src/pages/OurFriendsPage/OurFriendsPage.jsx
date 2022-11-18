7ccafb9b5e9d7fe3d82c34c8cc58dc18585a16d8
Serhii Okuniev <sergiosis@ukr.net>
2022-11-18 16:28:13 +0200

    last

--- src/pages/OurFriendsPage/OurFriendsPage.jsx
+++ src/pages/OurFriendsPage/OurFriendsPage.jsx
@@ -1,19 +1,26 @@
 import { fetchFriends } from 'api/friendsApi';
 import { useEffect, useState } from 'react';
+import Error from 'components/Common/Error/Error';
+import Loading from 'components/Common/Loding/Loading';
 import { Anchor, CardThumb, Container, FirstThumb, FriendsThumb, FriendTitle, Image, Item, SecondThumb, Title } from "./OurFriendsPage.styled";
 
-const OurFriendsPage = () => { 
+const OurFriendsPage = () => {
     
-    const [arr, setArr] = useState([])
+    const [arr, setArr] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
 
     const getFriends = async () => {
         try {
+            setIsLoading(true);
             const res = await fetchFriends();
             setArr(res);
         } catch (err) {
-            console.log(err.message);
+            setError(err);
+        } finally {
+            setIsLoading(false);
         }
-    }
+    };
 
     useEffect(() => {
         getFriends();
@@ -21,56 +28,65 @@ const OurFriendsPage = () => {
 
     return (
         <>
-            <Title>Our friends</Title>
-            
-            <FriendsThumb>
+            {arr && (<>
+                <Title>Our friends</Title>
+                
+                <FriendsThumb>
 
-                {arr.map(({ _id, imageUrl, title, time, address, email, phone }) => (
-                  
-                    <Container key={_id}>
-                        <FriendTitle>{title}</FriendTitle>
+                    {arr.map(({ _id, imageUrl, title, time, address, email, phone }) => (
+                    
+                        <Container key={_id}>
+                            <FriendTitle>{title}</FriendTitle>
 
-                        <CardThumb>
-                            <FirstThumb>
-                                <Image src={imageUrl} alt={`${title} img`} />
-                            </FirstThumb>
+                            <CardThumb>
+                                <FirstThumb>
+                                    <Image src={imageUrl} alt={`${title} img`} />
+                                </FirstThumb>
 
-                            <SecondThumb>
-                                
-                                <ul>
-                                    <Item >Time: <br />
-                                        <div className='time'>
-                                        {
-                                            time ? (<span>{time}</span>) : (<span>----------</span>)
+                                <SecondThumb>
+                                    
+                                    <ul>
+                                        <Item >Time: <br />
+                                            <div className='time'>
+                                                {
+                                                    time ? (<span>{time}</span>) : (<span>----------</span>)
+                                                }
+                                            </div>
+                                        </Item>
+                                        <Item>Adress:<br />
+                                            {
+                                                address ? (<span>{address}</span>) : (<span>----------</span>)
                                             }
-                                        </div>
-                                    </Item>
-                                    <Item>Adress:<br/>
-                                        {
-                                            address ? (<span>{address}</span>) : (<span>----------</span>)
-                                        }
-                                    </Item>
-                                    <Item>Email:<br/>
-                                        {
-                                            email ? (<Anchor href={`mailto:${email}`}>{email}</Anchor>) : (<span>----------</span>)
-                                        }
-                                    </Item>
-                                    <Item>Phone:<br/>
-                                        {
-                                            phone ? (<Anchor href={`tel:${phone}`}>{phone}</Anchor>) : (<span>----------</span>)
-                                        }
-                                    </Item>
-                                </ul>
+                                        </Item>
+                                        <Item>Email:<br />
+                                            {
+                                                email ? (<Anchor href={`mailto:${email}`}>{email}</Anchor>) : (<span>----------</span>)
+                                            }
+                                        </Item>
+                                        <Item>Phone:<br />
+                                            {
+                                                phone ? (<Anchor href={`tel:${phone}`}>{phone}</Anchor>) : (<span>----------</span>)
+                                            }
+                                        </Item>
+                                    </ul>
 
-                            </SecondThumb>
-                        </CardThumb>    
-                    </Container>
-                
-                ))
-                }
-                
-            </FriendsThumb>
+                                </SecondThumb>
+                            </CardThumb>
+                        </Container>
+                    
+                    ))
+                    }
+                    
+
+                </FriendsThumb>
+            </>
+            )}
+
+            {isLoading && <Loading/>}
+
+            {error && <Error/>}
         </>
+   
     )
 }
 
