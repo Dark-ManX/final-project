4b28158463979ceafa904e8499292a15a2e9c54c
Oleksandr <alexandrokrk@gmail.com>
2022-11-24 10:19:23 +0200

    1

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,9 +1,8 @@
-import RegistrationDetails from 'pages/RegisterPageDetails/RegisterPageDetails';
+
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useRegisterUserMutation ,useAddUserMutation} from 'redux/auth/authOperations';
 import { Link, useLocation ,useNavigate} from 'react-router-dom';
-import AuthForm from 'components/AuthForm';
 import {
   Title,
   FirstContainer,
@@ -29,10 +28,7 @@ const RegisterPage = () => {
   const [city, setCity] = useState('');
   const [phone, setPhone] = useState('');
   const [page, setPage] = useState(false)
-  // const [id, setId]=useState(null)
   const isId = useSelector(state => state.auth.user.id);
-//   console.log(isId)
-// console.log(email,password,confirmPassword)
  const [passwordShown, setPasswordShown] = useState(true);
 
   const togglePassword = () => {
@@ -260,43 +256,7 @@ const addUserInfo = async () => {
 
           </FirstContainer>
         </ImageContainer>
-</Section>
-
-
-            {/* </FirstContainer>
-          )}
-          {page > 0 && (
-            <Container>
-              <Title>Registration</Title>
-
-              <Form>
-                {conditionalComponent()}
-                <ul>
-                  <li>
-                    <Button onClick={handleSubmit}>
-                      {page === 0 || page < 1 ? 'Next' : 'Register'}
-                    </Button>
-                  </li>
-                  {page > 0 && (
-                    <li>
-                      <BackBtn onClick={() => setPage(page - 1)}>Back</BackBtn>
-                    </li>
-                  )}
-                  <li>
-                    <P>
-                      Already have an account?
-                      <Link to={`/login`} state={{ from: location }}>
-                        <Span>Login </Span>
-                      </Link>
-                    </P>
-                  </li>
-                </ul>
-              </Form>
-            </Container>
-          )}
-
-        </ImageContainer>
-      </Section> */}
+      </Section>
     </>
   );
 };
