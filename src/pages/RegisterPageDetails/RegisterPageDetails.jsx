1ec5156efc6293783c2947a32e7d18a1ce2ea586
Oleksandr <alexandrokrk@gmail.com>
2022-11-26 17:11:32 +0200

    1

--- src/pages/RegisterPageDetails/RegisterPageDetails.jsx
+++ src/pages/RegisterPageDetails/RegisterPageDetails.jsx
@@ -1,46 +1,48 @@
-import { Input } from './RegisterPageDetails.styled';
-import { useSelector } from 'react-redux';
+// import { Input } from './RegisterPageDetails.styled';
+// import { useSelector } from 'react-redux';
 
-const RegistrationDetails = ({ formData, setFormData }) => {
-  const isId = useSelector(state => state.auth.user.id);
-  console.log(isId)
-  return (
-    <>
-      <Input
-        type="text"
-        onChange={e => {
-          setFormData({
-            ...formData,
-            name: e.target.value,
-          });
-        }}
-        value={formData.name}
-        placeholder="Name"
-      />
-      <Input
-        type="text"
-        onChange={e => {
-          setFormData({
-            ...formData,
-            city: e.target.value,
-          });
-        }}
-        value={formData.city}
-        placeholder="City, region"
-      />
-      <Input
-        type="tel"
-        onChange={e => {
-          setFormData({
-            ...formData,
-            phone: e.target.value,
-          });
-        }}
-        value={formData.phone}
-        placeholder="Mobile phone"
-      />
-    </>
-  );
-};
 
-export default RegistrationDetails;
+// const RegistrationDetails = ({ formData, setFormData }) => {
+//   const isId = useSelector(state => state.auth.user.id);
+//   console.log(isId)
+
+//   return (
+//     <>
+//       <Input
+//         type="text"
+//         // onChange={e => {
+//         //   setFormData({
+//         //     ...formData,
+//         //     name: e.target.value,
+//         //   });
+//         // }}
+//         value={name}
+//         placeholder="Name"
+//       />
+//       <Input
+//         type="text"
+//         // onChange={e => {
+//         //   setFormData({
+//         //     ...formData,
+//         //     city: e.target.value,
+//         //   });
+//         // }}
+//         value={city}
+//         placeholder="City, region"
+//       />
+//       <Input
+//         type="tel"
+//         // onChange={e => {
+//         //   setFormData({
+//         //     ...formData,
+//         //     phone: e.target.value,
+//         //   });
+//         // }}
+//         value={phone}
+//         placeholder="Mobile phone"
+//       />
+//     </>
+//   );
+// };
+
+// export default RegistrationDetails;
