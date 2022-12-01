0541c0ad82ca77aa06ef90dadf407a8e6d9b2669
OksanaOstafiichuk <Ostafiichukk@gmail.com>
2022-12-01 18:37:51 +0200

    remove console.log

--- src/components/User/UserData/UserData.jsx
+++ src/components/User/UserData/UserData.jsx
@@ -1,9 +1,6 @@
 import PropTypes from 'prop-types';
 import { useEffect, useState, useRef } from 'react';
-import {
-  useGetUserInfoQuery,
-  useUpdateAvatarMutation,
-} from 'redux/auth/authOperations';
+import { useUpdateAvatarMutation } from 'redux/auth/authOperations';
 import { UserDataItem } from 'components/User/UserDataItem/UserDataItem';
 import editPhoto from 'icons/editPhoto.svg';
 import { Avatar, EditPhotoBtn, ImgUser, UserInfo } from './UserData.styled';
@@ -12,37 +9,29 @@ import { response } from 'api';
 import { useSelector } from 'react-redux';
 
 export const UserData = () => {
-  const [file, setFile] = useState({});
   const [user, setUser] = useState([]);
   const [logo, setLogo] = useState('');
-  const getUserInfo = useGetUserInfoQuery();
   const [updateAvatar] = useUpdateAvatarMutation();
 
   const { getUser } = response;
 
   const token = useSelector(state => state.auth.token);
-  console.log(token);
 
   const fetchUser = async token => {
     const res = await getUser(token);
-    console.log(res);
     setUser(res);
-    console.log(res.logo);
     setLogo(res.logo);
   };
 
-  console.log(user);
-
   const handleChangeAvatar = async evt => {
-    console.log(evt.target.files[0]);
-    console.log('Click');
-
     const formData = new FormData();
 
     formData.append('avatar', evt.target.files[0]);
 
-    const { data } = await updateAvatar(formData);
-    setLogo(data.avatarURL);
+    const {
+      data: { avatarURL },
+    } = await updateAvatar(formData);
+    setLogo(avatarURL);
   };
 
   useEffect(() => {
@@ -67,7 +56,7 @@ export const UserData = () => {
           </EditPhotoBtn>
         </label>
       </Avatar>
-      <UserDataItem user={user} />
+      <UserDataItem />
     </UserInfo>
   );
 };
