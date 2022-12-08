6f5a28b7a04cfa96ee183e095c5324df09dfaff6
Serhii Okuniev <sergiosis@ukr.net>
2022-12-08 13:54:47 +0200

    wrap userData into try-catch

--- src/components/User/UserData/UserData.jsx
+++ src/components/User/UserData/UserData.jsx
@@ -1,20 +1,22 @@
 import PropTypes from 'prop-types';
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-
-import { useUpdateAvatarMutation } from 'redux/auth/authOperations';
+import { response } from 'api';
+import Error from 'components/Error/Error';
 import UserDataItem from 'components/User/UserDataItem/UserDataItem';
 import editPhoto from 'icons/editPhoto.svg';
-import defaultImg from '../../../img/defaultImg.jpg';
-import { ROUTES } from 'routes/routes';
-import { response } from 'api';
+import defaultImg from 'img/defaultImg.jpg';
+import { useUpdateAvatarMutation } from 'redux/auth/authOperations';
 
 import { Avatar, EditPhotoBtn, ImgUser, UserInfo } from './UserData.styled';
 
 const UserData = () => {
+  const [error, setError] = useState(false);
   const [user, setUser] = useState([]);
-  const [logo, setLogo] = useState('');
+  const [avatar, setAvatar] = useState('');
   const [updateAvatar] = useUpdateAvatarMutation();
+
+  console.log('user', avatar);
 
   const { getUser } = response;
 
@@ -24,50 +26,64 @@ const UserData = () => {
     const formData = new FormData();
 
     formData.append('avatar', evt.target.files[0]);
-
+    console.log(formData);
     const {
       data: { avatarURL },
-    } = await updateAvatar(formData);
-    setLogo(avatarURL);
+    } =
+      // console.log('formData :>> ', formData);
+      await updateAvatar(formData);
+
+    // console.log(avatarURL);
+    // setAvatar(avatarURL);
   };
 
   useEffect(() => {
     const fetchUser = async token => {
-      const res = await getUser(token);
-      setUser(res);
-      setLogo(res.logo);
+      try {
+        const res = await getUser(token);
+        setUser(res);
+      } catch (err) {
+        setError(true);
+      }
+      // setAvatar(res.logo);
     };
 
     fetchUser(token);
   }, [getUser, token]);
 
-  const avatarUser = () => {
-    if (logo.includes('https://s.gravatar.com')) {
-      return defaultImg;
-    } else {
-      return `${ROUTES.BASE_URL}/${logo}`;
-    }
-  };
+  // const avatarUser = () => {
+  //   if (logo.includes('https://s.gravatar.com')) {
+  //     return defaultImg;
+  //   } else {
+  //     return `${ROUTES.BASE_URL}/${logo}`;
+  //   }
+  // };
 
   return (
     <UserInfo>
-      <Avatar>
-        <ImgUser src={avatarUser()} alt={user.name} />
-        <label>
-          <input
-            type="file"
-            accept="image/png, image/gif, image/jpeg"
-            name="image"
-            onChange={handleChangeAvatar}
-            style={{ display: 'none' }}
-          />
-          <EditPhotoBtn>
-            <img src={editPhoto} alt="addPet" />
-            Edit photo
-          </EditPhotoBtn>
-        </label>
-      </Avatar>
-      <UserDataItem />
+      {!error ? (
+        <>
+          <Avatar>
+            <ImgUser src={user.logo ? user.logo : defaultImg} alt={user.name} />
+            <label>
+              <input
+                type="file"
+                accept="image/png, image/gif, image/jpeg"
+                name="image"
+                onChange={handleChangeAvatar}
+                style={{ display: 'none' }}
+              />
+              <EditPhotoBtn>
+                <img src={editPhoto} alt="addPet" />
+                Edit photo
+              </EditPhotoBtn>
+            </label>
+          </Avatar>
+          <UserDataItem />
+        </>
+      ) : (
+        <Error />
+      )}
     </UserInfo>
   );
 };
