61b72aaff4fd58335b8b662413fb0bf05d90b06b
Serhii Okuniev <sergiosis@ukr.net>
2022-12-12 00:58:34 +0200

    try to deploy

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App/App';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import { store, persistor } from './redux/store';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
+import App from './components/App/App';
+import './index.css';
+import { persistor, store } from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
